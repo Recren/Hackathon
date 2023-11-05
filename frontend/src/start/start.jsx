4e526d2f61e6249699cf15b7084bcbc37c6c15e3
ericwang210000 <exw210000@utdallas.edu>
2023-11-04 19:50:52 -0500

    Name change

--- frontend/src/start/start.jsx
+++ frontend/src/start/start.jsx
@@ -1,18 +1,88 @@
 import React, {useState} from 'react';
 
-async function getUniName(){
+function getUniList(){
     fetch('world_universities_and_domains.json')
     .then(response => {
         const data = response.data.items.filter((item) => item.country == 'United States');
-        let dataElements = [["", ""], ["", ""], ["", ""], ["", ""], ["", ""]];
-        data.forEach((item, index) => {
-            dataElements[index] = String(item.name);
-        });
-  
-        return dataElements;
+        return data;
     })
 }
+const uniList = getUniList();
 
-function start(){
+function Start(){
+    const [items, setItems] = useState([uniList]);
+    const [query, setQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedItem, setSelectedItem] = useState([]);
 
+     //handle search bar input change
+    const handleInputChange = (e) => {
+        const inputValue = e.target.value;
+        setQuery(inputValue);
+        handleSearch(inputValue); // Call the provided callback with the query
+    };
+    
+    //cursor 
+    const cursorStyle = { cursor: 'pointer'};
+    
+    let dataset = [["", ""], ["", ""], ["", ""], ["", ""], ["", ""]];
+    // Define a callback function to handle search
+    const handleSearch = async (query) => {
+        try{
+            if(query === ""){
+                dataset = [""];
+            } else {
+                const filteredItems = items.filter((item) =>
+                    item.toLowerCase().includes(searchQuery.toLowerCase())
+                );
+                setSearchResults(dataset);
+            }
+            
+        }catch(error){
+            throw error;
+        }
+    }
+
+    // Define functions to handle input changes
+    const handleItemClick = (item) => {
+        setSelectedItem(item);
+    };
+
+    return(
+        <div>
+            <div className="search-container" style={cursorStyle}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={query}
+                    onChange={handleInputChange}
+                    onFocus={() => {
+                        setQuery(''); // Clear the search bar input
+                        // Clear the search results when focusing on the search bar
+                        setSearchResults([]);
+                    }}          
+                />
+            </div>
+
+            {/* Display search results */}
+            {searchResults.length > 0 && (
+                <div className='search-results'>
+                    <ul>
+                        {searchResults.map((result, index) => (
+                        <li
+                            key={index}
+                            onClick={() => handleItemClick(result)}
+                            className={selectedItem === result ? 'selected' : ''}
+                        >
+                            {result}
+                        </li>
+                        ))}
+                    </ul>
+                    {selectedItem && <p>School: {selectedItem}</p>}
+                </div>
+            )} 
+        </div> 
+    );
 }
+
+export default Start;
