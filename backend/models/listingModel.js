ca3233f7df414a05bd021ecee4a60bab2a2598b2
Steven <stevenvo777@gmail.com>
2023-11-04 19:07:17 -0500

    Implemented POST api to allow users to post listings to website

---
+++ backend/models/listingModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//category refers to what kind of product it is
+//(tech, appliances, clothes, furniture, etc.)
+//product is more specific(tech -> laptop,phone,etc)
+//Structure of document
+const listingSchema = new Schema(
+  {
+    itemName: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    product: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    condition: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    views: {
+      type: Number,
+    },
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//Creates a model of the listing
+module.exports = mongoose.model("Listing", listingSchema);
--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const Listing = require("../models/listingModel");
 const router = express.Router();
 
 //GET all the listings
@@ -13,8 +13,32 @@ router.get("/:id", (req, res) => {
 });
 
 //POST a new listing
-router.post("/", (req, res) => {
-  res.json({ mssg: "POST a new listing" });
+router.post("/", async (req, res) => {
+  const {
+    itemName,
+    description,
+    category,
+    product,
+    price,
+    condition,
+    views,
+    user,
+  } = req.body;
+  try {
+    const listing = await Listing.create({
+      itemName,
+      description,
+      category,
+      product,
+      price,
+      condition,
+      views,
+      user,
+    });
+    res.status(200).json(listing);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 //DELETE a lisitng
