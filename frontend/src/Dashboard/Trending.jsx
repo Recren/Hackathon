2c83b4614751029a08bb1e7b8adea25ed2eee4c3
ericwang210000 <exw210000@utdallas.edu>
2023-11-05 08:25:37 -0600

    Search bar and API are doneeee ish? I am so tired. the database works tho. yay.

--- frontend/src/Dashboard/Trending.jsx
+++ frontend/src/Dashboard/Trending.jsx
@@ -1,15 +1,28 @@
 import React, {useState} from 'react';
 import Header from './Header';
 
+function ItemCard({ name, description, price, condition }) {
+    return (
+      <div className="item-card">
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <p>Price: ${price}</p>
+        <p>Condition: {condition}</p>
+      </div>
+    );
+  }
+
 function Trending(){
-    const [items, setItems] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [query, setQuery] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+  
 
     async function getInventory(){
         return fetch('http://localhost:4000/api/listings')
         .then((response) => response.json())
         .then((items) => {
-          console.log(items)
-          setItems(items);
+          setProducts(items);
         })
         .catch((error) => {
           console.error('Error fetching data:', error);
@@ -17,9 +30,63 @@ function Trending(){
     }
 
     getInventory();
+  
+     //handle search bar input change
+     const handleInputChange = (e) => {
+        const inputValue = e.target.value;
+        setQuery(inputValue);
+        handleSearch(inputValue); // Call the provided callback with the query
+    };
+    
+    //cursor 
+    const cursorStyle = { cursor: 'pointer'};
+    
+    let dataset = [];
+    // Define a callback function to handle search
+    const handleSearch = async (query) => {
+        try{
+            if(query === ""){
+                dataset = [];
+            } else {
+                let filteredItems = products.filter((item) =>
+                    item.itemName.toLowerCase().includes(query.toLowerCase())
+                );
+               console.log(filteredItems);
+                setProducts(filteredItems);
+            }
+            
+        }catch(error){
+            throw error;
+        }
+    }
+
     return(
-        <Header />
+        <div>
+            <Header/>
+            <div style={cursorStyle}>
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={query}
+                        onChange={handleInputChange}
+                        onFocus={() => {
+                            setQuery(''); // Clear the search bar input
+                            // Clear the search results when focusing on the search bar
+                            setSearchResults([]);
+                        }}          
+                    />
+                </div>
+                
+                <div className="array-container">
+                    {products.map((item, index) => (
+                        <ItemCard key={index} name ={item.itemName} description = {item.description} price = {item.price} condition = {item.condition}/>
+                    ))}
+                </div>
+            </div>
+        </div> 
     );
-} 
+}
+
 
 export default Trending;
